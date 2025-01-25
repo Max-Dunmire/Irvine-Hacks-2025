cd1303729a46f63493c7b4cb2e96779fc4cbfdec
Max Dunmire <147551026+Max-Dunmire@users.noreply.github.com>
2025-01-24 23:58:29 -0800

    Update TESTING_NN.py
    Working NN with MNIST Dataset

--- TESTING_NN.py
+++ TESTING_NN.py
@@ -1,1 +1,109 @@
-import pytorch sbaslhto
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torchvision.utils import make_grid
+
+import numpy as np
+import pandas as pd
+
+transform = transforms.ToTensor()
+
+# importing the dataset (MNIST numbers for now) for training
+train_data = datasets.MNIST(root="/cnn_data", train=True, download=True, transform=transform)
+
+# importing dataset for testing
+test_data = datasets.MNIST(root="/cnn_data", train=False, download=True, transform=transform)
+
+# create batches
+train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=10, shuffle=False)
+
+# class
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(5*5*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        # Second pass
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        
+        # Flatten
+        X = X.view(-1, 16*5*5)
+
+        # Fully connected layers
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+
+        # return
+        return F.log_softmax(X, dim=1)
+    
+torch.manual_seed(41)
+
+model = ConvolutionalNetwork()
+
+# Loss Function Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001) # lr = learning rate
+
+
+# testing block
+import time
+start_time = time.time()
+
+epochs = 5
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+for i in range(epochs):
+    trn_corr = 0
+    tst_corr = 0
+
+    # train
+    for b, (X_train, Y_train) in enumerate(train_loader):
+        b+=1
+        y_pred = model(X_train)
+        loss = criterion(y_pred, Y_train)
+
+        predicted = torch.max(y_pred.data, 1)[1]
+        batch_corr = (predicted == Y_train).sum()
+        trn_corr += batch_corr
+
+        # update params
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if b%600 == 0:
+            print(f'Epoch: {i} Batch: {b} Loss: {loss.item()}')
+
+    train_losses.append(loss)
+    train_correct.append(trn_corr)
+
+
+    with torch.no_grad():
+        for b,(X_test, Y_test) in enumerate(test_loader):
+            y_val = model(X_test)
+            predicted = torch.max(y_val.data, 1)[1]
+            tst_corr += (predicted == Y_test).sum()
+
+    loss = criterion(y_val, Y_test)
+    test_losses.append(loss)
+    test_correct.append(tst_corr)
+
+
+current_time = time.time()
+total = current_time - start_time
+print(f'training took {total/60} minutes')
